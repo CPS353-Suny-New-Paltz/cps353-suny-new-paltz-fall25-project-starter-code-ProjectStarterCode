bf6a512ecbc646165ed56eeb5102d44325c34cc5
tiripicj1 <jtiripicchio@gmail.com>
2025-09-21 19:32:19 -0400

    Update HelloBroken.java

--- src/broken/HelloBroken.java
+++ src/broken/HelloBroken.java
@@ -1,5 +1,6 @@
-package broken;
-
+package broken; // ← tab before 'package' to trigger style
 public class HelloBroken {
-
-}
+    public static void main(String[] args) {
+        System.out.println("hi")   // ← missing semicolon
+    }
+} // ← trailing space
